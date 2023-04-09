f45011d6947188f6c63b8203e275cfc46fde7746
Karina Grishaeva <revuwem@gmail.com>
2023-04-09 22:13:59 +0400

    feat(components): create Card layout

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,54 @@
-type CardProps = {};
+type CardProps = {
+  title: string;
+  icon: string;
+  color: "orange" | "blue" | "pink" | "green" | "violet" | "yellow";
+  period: "day" | "week" | "month";
+  currentTime: number;
+  previousTime: number;
+};
 
-const Card: React.FC<CardProps> = ({}) => {
-  return <div>Card</div>;
+const bgColor = {
+  orange: "bg-cards-orange",
+  blue: "bg-cards-blue",
+  pink: "bg-cards-pink",
+  green: "bg-cards-green",
+  violet: "bg-cards-violet",
+  yellow: "bg-cards-yellow",
+};
+
+const Card: React.FC<CardProps> = ({
+  title,
+  icon,
+  color,
+  period,
+  currentTime,
+  previousTime,
+}) => {
+  return (
+    <div>
+      {/* FIX: bg color not working if passed throw props */}
+      <div
+        className={`px-4 ${bgColor[color]} rounded flex justify-end translate-y-6`}
+      >
+        <img src={icon} alt="" />
+      </div>
+      <section className="relative p-4 md:p-8 bg-blue-dark rounded z-10 hover:bg-blue-darken hover:cursor-pointer">
+        <div className="flex justify-between mb-6">
+          <h3 className="font-medium text-lg text-white">{title}</h3>
+          {/* TODO: replace with Dropdown */}
+          <span className="text-white">...</span>
+        </div>
+        <div className="flex md:flex-col justify-between gap-1.5">
+          <p className="font-light text-4xl md:text-6xl text-white">
+            {currentTime}hrs
+          </p>
+          <p className="font-light text-capitalize text-base text-blue-pale">
+            Last {period} - {previousTime}hrs
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Card;
