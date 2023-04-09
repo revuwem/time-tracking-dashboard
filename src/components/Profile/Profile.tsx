0ce7b8da803b10892e416d364a3c9ba10a21fe95
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 02:15:48 +0400

    feat(components): add responsive styles at Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,11 +5,11 @@ type ProfileProps = {
 
 const Profile: React.FC<ProfileProps> = ({ name, photo }) => {
   return (
-    <div className="px-8 py-9 bg-blue-light rounded flex flex-wrap items-center gap-5 md:flex-col md:items-start md:gap-10">
+    <div className="h-full px-8 py-9 bg-blue-light rounded flex flex-wrap items-center gap-5 lg:flex-col lg:items-start md:gap-10">
       <div className="w-16 md:w-20 rounded-full border-2 border-white">
         <img src={photo} alt="Your photo" className="object-cover" />
       </div>
-      <h1 className="font-light text-2xl md:text-5xl text-white flex flex-col md:gap-1">
+      <h1 className="font-light text-2xl md:text-3xl lg:text-5xl text-white flex flex-col md:gap-1">
         <span className="text-sm md:text-base text-blue-pale">Report for</span>
         {name}
       </h1>
