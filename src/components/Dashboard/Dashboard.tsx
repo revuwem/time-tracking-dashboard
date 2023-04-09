969c1dace77dcfa2ee6d2f045d60b46a2df9289d
Karina Grishaeva <revuwem@gmail.com>
2023-04-09 22:14:25 +0400

    feat(components): add props for Cards at Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,9 +5,30 @@ type DashboardProps = {};
 const Dashboard: React.FC<DashboardProps> = ({}) => {
   return (
     <div>
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        title="Work"
+        icon="images/icon-work.svg"
+        color="orange"
+        period="day"
+        currentTime={32}
+        previousTime={24}
+      />
+      <Card
+        title="Play"
+        icon="images/icon-play.svg"
+        color="blue"
+        period="day"
+        currentTime={10}
+        previousTime={8}
+      />
+      <Card
+        title="Study"
+        icon="images/icon-study.svg"
+        color="pink"
+        period="day"
+        currentTime={4}
+        previousTime={6}
+      />
     </div>
   );
 };
