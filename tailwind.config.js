7eb6362bd679862d90eeeb7254cd0e74d26675f2
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 02:10:43 +0400

    feat(tailwindcss): add line-height for 5xl font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       fontSize: {
         "4xl": "2rem",
-        "5xl": "2.5rem",
+        "5xl": ["2.5rem", "125%"],
         "6xl": "3.5rem",
       },
       borderRadius: {
