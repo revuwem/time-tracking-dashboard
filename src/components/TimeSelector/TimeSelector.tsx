35c68f04aafc01adeccbbcee747d234d1cf1d15b
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 02:16:18 +0400

    feat(components): change styles at TimeSelector

--- src/components/TimeSelector/TimeSelector.tsx
+++ src/components/TimeSelector/TimeSelector.tsx
@@ -4,8 +4,8 @@ type TimeSelectorProps = {};
 
 const TimeSelector: React.FC<TimeSelectorProps> = ({}) => {
   return (
-    <nav className="p-8 bg-blue-dark rounded">
-      <ul className="grid gap-5">
+    <nav className="p-8 pt-12 bg-blue-dark rounded">
+      <ul className="flex justify-center md:flex-col gap-5">
         <TimeSelectorItem>Daily</TimeSelectorItem>
         <TimeSelectorItem>Weekly</TimeSelectorItem>
         <TimeSelectorItem>Monthly</TimeSelectorItem>
