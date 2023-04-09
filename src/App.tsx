9eaef0cab2db0998b3e873e8999db307c3b9289e
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 02:18:05 +0400

    feat(App): style App layout grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import TimeSelector from "./components/TimeSelector";
 
 function App() {
   return (
-    <main>
-      <Profile name="Jeremy Dawson" photo="images/image-jeremy.png" />
-      <TimeSelector />
-      <Dashboard />
+    <main className="min-h-screen px-5 py-10 bg-black grid place-content-center lg:place-content-stretch lg:place-items-center">
+      <div className="w-full max-w-[1100px] grid gap-8 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-2 lg:grid-flow-col items-end lg:items-stretch">
+        <div className="row-span-full flex flex-col">
+          <div className="grow z-10">
+            <Profile name="Jeremy Dawson" photo="images/image-jeremy.png" />
+          </div>
+          <div className="-mt-6">
+            <TimeSelector />
+          </div>
+        </div>
+        <Dashboard />
+      </div>
     </main>
   );
 }
