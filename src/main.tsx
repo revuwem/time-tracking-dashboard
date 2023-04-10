c0d7e9eeb6fa6bf73b6733fff65508417d03728f
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 20:52:40 +0400

    feat: add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider as StoreProvider } from "react-redux";
+import { store } from "./store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
