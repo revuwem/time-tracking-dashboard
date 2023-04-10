3df8db96c26dd84ead4ff265f4ea514d94b98690
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 21:34:44 +0400

    feat(components): update Card props and use props at render

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,13 @@
+import {
+  TimeCardColor,
+  TimePeriod,
+  TimeTracking,
+} from "../../types/TimeTracking";
 import Dropdown from "../Dropdown";
 
 type CardProps = {
-  title: string;
-  icon: string;
-  color: "orange" | "blue" | "pink" | "green" | "violet" | "yellow";
-  period: "day" | "week" | "month";
-  currentTime: number;
-  previousTime: number;
+  data: TimeTracking;
+  period: TimePeriod;
 };
 
 const bgColor = {
@@ -18,23 +19,16 @@ const bgColor = {
   yellow: "bg-cards-yellow",
 };
 
-const Card: React.FC<CardProps> = ({
-  title,
-  icon,
-  color,
-  period,
-  currentTime,
-  previousTime,
-}) => {
+const Card: React.FC<CardProps> = ({ data, period }) => {
   return (
     <div>
       {/* FIX: bg color not working if passed throw props */}
-      <div className={`px-4 ${bgColor[color]} rounded flex justify-end`}>
-        <img src={icon} alt="" />
+      <div className={`px-4 ${bgColor[data.color]} rounded flex justify-end`}>
+        <img src={data.icon} alt="" />
       </div>
       <section className="relative p-4 md:p-7 -mt-7 bg-blue-dark rounded z-10 hover:bg-blue-darken hover:cursor-pointer">
         <div className="flex justify-between mb-6">
-          <h3 className="font-medium text-lg text-white">{title}</h3>
+          <h3 className="font-medium text-lg text-white">{data.title}</h3>
           <Dropdown>
             <Dropdown.Item>
               <button onClick={() => console.log("reset current")}>
@@ -55,10 +49,10 @@ const Card: React.FC<CardProps> = ({
         </div>
         <div className="flex md:flex-col items-center md:items-start justify-between gap-1.5">
           <p className="font-light text-4xl md:text-6xl text-white">
-            {currentTime}hrs
+            {data.timeframes[period].current}hrs
           </p>
           <p className="font-light text-capitalize text-base text-blue-pale">
-            Last {period} - {previousTime}hrs
+            Last {period} - {data.timeframes[period].previous}hrs
           </p>
         </div>
       </section>
