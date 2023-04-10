f720290f915b32fa248bcb435b18b9114ef5fd62
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 22:22:49 +0400

    feat(components): add TimeSelector functionality

--- src/components/TimeSelector/TimeSelector.tsx
+++ src/components/TimeSelector/TimeSelector.tsx
@@ -1,14 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { setPeriod } from "../../store/slice/timeTracking";
+import { TimePeriod } from "../../types/TimeTracking";
 import TimeSelectorItem from "../TimeSelectorItem";
 
 type TimeSelectorProps = {};
 
+type TimeOptions = { [k in TimePeriod]: { name: string } };
+
+const timeOptions: TimeOptions = {
+  daily: {
+    name: "Daily",
+  },
+  weekly: {
+    name: "Weekly",
+  },
+  monthly: {
+    name: "Monthly",
+  },
+};
+
 const TimeSelector: React.FC<TimeSelectorProps> = ({}) => {
+  const dispatch = useDispatch();
+  const period = useSelector<RootState, TimePeriod>(
+    (state) => state.timeTracking.period
+  );
+
+  const onChangePeriod = (period: TimePeriod) => dispatch(setPeriod(period));
+
   return (
     <nav className="p-8 pt-12 bg-blue-dark rounded">
       <ul className="flex justify-center md:flex-col gap-5">
-        <TimeSelectorItem>Daily</TimeSelectorItem>
-        <TimeSelectorItem>Weekly</TimeSelectorItem>
-        <TimeSelectorItem>Monthly</TimeSelectorItem>
+        {Object.keys(timeOptions).map((option) => (
+          <TimeSelectorItem active={period === option}>
+            <button onClick={(e) => onChangePeriod(option as TimePeriod)}>
+              {timeOptions[option as TimePeriod].name}
+            </button>
+          </TimeSelectorItem>
+        ))}
       </ul>
     </nav>
   );
