665f3949cb9550181ef78d0b39d393c77173b0ff
Karina Grishaeva <revuwem@gmail.com>
2023-04-10 21:34:07 +0400

    feat(types): add 'icon' and 'color' to TimeTracking type

--- src/types/TimeTracking.ts
+++ src/types/TimeTracking.ts
@@ -4,8 +4,17 @@ type TimeTrack = {
 };
 
 export type TimePeriod = "daily" | "weekly" | "monthly";
+export type TimeCardColor =
+  | "orange"
+  | "blue"
+  | "pink"
+  | "green"
+  | "violet"
+  | "yellow";
 
 export type TimeTracking = {
   title: "string";
+  icon: string;
+  color: TimeCardColor;
   timeframes: { [k in TimePeriod]: TimeTrack };
 };
