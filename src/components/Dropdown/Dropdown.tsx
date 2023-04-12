5c4d2a58ba830cad7b996f8fd0a165e0299f213e
Karina Grishaeva <revuwem@gmail.com>
2023-04-12 16:10:21 +0400

    feat(components): add close when clicked outside feature to Dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "usehooks-ts";
 
 type DropdownProps = React.PropsWithChildren & {};
 type DropdownComponent = React.FC<DropdownProps> & {
@@ -6,11 +7,13 @@ type DropdownComponent = React.FC<DropdownProps> & {
 };
 
 const Dropdown: DropdownComponent = ({ children }) => {
+  const ref = useRef(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const onDropdownClick = () => setIsOpen((state) => !state);
 
-  // TODO: dropdown list should hide when user clicks outside of the dropdown
+  useOnClickOutside(ref, () => setIsOpen(false));
+
   return (
     <div className="relative">
       <button onClick={onDropdownClick} className="p-3">
@@ -23,7 +26,7 @@ const Dropdown: DropdownComponent = ({ children }) => {
         </svg>
       </button>
       {isOpen && (
-        <div className="absolute z-20 right-0">
+        <div className="absolute z-20 right-0" ref={ref}>
           <ul
             className="p-5 flex flex-col bg-blue-light rounded whitespace-nowrap"
             role="menu"
