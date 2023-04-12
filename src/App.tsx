f55fb7cfc0ff3cbbeceb7b5a9f1d5c1b5c635cac
Karina Grishaeva <revuwem@gmail.com>
2023-04-12 21:43:04 +0400

    feat(App): add dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 import { fetchTimeTrackingData } from "./store/slice/timeTracking";
+import { useDarkMode } from "usehooks-ts";
 import { TimeTracking } from "./types/TimeTracking";
 import { Loading } from "./types/AppState";
 import Profile from "./components/Profile";
@@ -22,11 +23,22 @@ function App() {
     dispatch(fetchTimeTrackingData());
   }, []);
 
+  const { isDarkMode, toggle } = useDarkMode();
+
   return (
-    <main className="min-h-screen px-5 py-10 bg-black grid place-content-center lg:place-content-stretch lg:place-items-center">
+    <main
+      className={`min-h-screen px-5 py-10 grid place-content-center lg:place-content-stretch lg:place-items-center ${
+        isDarkMode ? "dark bg-black" : "bg-gray"
+      } `}
+    >
       {loading === "pending" && (
-        <p className="text-white">Loading your productivity stats...</p>
+        <p className="text-black dark:text-white">
+          Loading your productivity stats...
+        </p>
       )}
+      <nav className="text-black dark:text-white">
+        <button onClick={toggle}>Set Light Mode</button>
+      </nav>
       {loading === "succeeded" && (
         <div className="w-full max-w-[1100px] grid gap-8 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-2 lg:grid-flow-col items-end lg:items-stretch">
           <div className="row-span-full flex flex-col">
